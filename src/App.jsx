9aeccf80040d7b0fbfa0ee4855ae9d1de306d548
Ireoluwa Okunnuga <jaywonlekxy@gmail.com>
2026-01-06 18:32:23 +0100

    Added Operation page, fragmentation, mockdata

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import CreationPage from "./pages/CreationPage";
 import LoginPage from "./pages/LoginPage";
 import MyBooks from "./components/students/MyBooks";
 import BookSearch from "./components/students/BookSearch/BookSearch";
+import Operation from "./pages/Operation";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/" element={<CreationPage />} />
         <Route path="/login-page" element={<LoginPage />} />
+        <Route path="/operaation" element={<Operation />} />
         <Route path="/students/my-books" element={<MyBooks />} />
         <Route path="/students/search" element={<BookSearch />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
